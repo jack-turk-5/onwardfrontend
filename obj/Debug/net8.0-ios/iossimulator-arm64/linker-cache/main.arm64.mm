91f20c2674aa7fb82d1f3b6f3d0697c08a36c938
Jack Turk <jackturk5@outlook.com>
2024-04-26 22:24:48 -0500

    missing debug target files for vscode

--- obj/Debug/net8.0-ios/iossimulator-arm64/linker-cache/main.arm64.mm
+++ obj/Debug/net8.0-ios/iossimulator-arm64/linker-cache/main.arm64.mm
@@ -60,7 +60,7 @@ void xamarin_setup_impl ()
 	xamarin_runtime_libraries = xamarin_runtime_libraries_array;
 	xamarin_gc_pump = FALSE;
 	xamarin_init_mono_debug = TRUE;
-	xamarin_executable_name = "ShellFlyoutSample.dll";
+	xamarin_executable_name = "Onward.dll";
 	mono_use_llvm = FALSE;
 	xamarin_log_level = 0;
 	xamarin_arch_name = "arm64";
