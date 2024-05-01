065961514fa2ec657dac9a95cbec4e216de22df5
Jack Turk <jackturk5@outlook.com>
2024-04-30 19:20:24 -0500

    ViewLineItems implemented

--- ViewLineItems.xaml.cs
+++ ViewLineItems.xaml.cs
@@ -1,9 +1,51 @@
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
 namespace Onward;
 
 public partial class ViewLineItems : ContentPage
 {
+	private readonly ServerSocket serverSocket;
+	private List<LineItem>? lineItemsDe;
+	private ObservableCollection<LineItem> lineItems;
+
+    public ObservableCollection<LineItem> LineItems
+	{  get { return lineItems; } 
+	   set { lineItems = value; }
+	}
 	public ViewLineItems()
 	{
-		InitializeComponent();
+        serverSocket = new();
+		lineItemsDe = [];
+		lineItems = [];
+		PopulateLineItems();
+		BindingContext = this;
+        InitializeComponent();
+	}
+
+	private async void PopulateLineItems()
+	{
+		var (json, success) = await serverSocket.GetAsync("/lineitems/getlineitems");
+		if(success)
+		{
+            lineItemsDe = JsonConvert.DeserializeObject<List<LineItem>>(json);
+			if (lineItemsDe != null && lineItemsDe.Count > 0)
+			{
+				foreach (LineItem lineItem in lineItemsDe)
+				{
+					lineItems.Add(lineItem);
+				}
+			}
+			else
+			{
+				await DisplayAlert("Error", "Line Item list is null", "Close");
+				await Navigation.PopAsync();
+            }
+		}
+		else 
+		{
+			await DisplayAlert("Error", "Line Item list can't be found: " + json, "Close");
+            await Navigation.PopAsync();
+        }
 	}
 }
